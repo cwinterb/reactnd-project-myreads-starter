0be28bf1b1c82138975cfbef744587ee4be56a26
Carmella Winterbauer <carmella@onehopewine.com>
2020-02-16 15:59:16 -0800

    wait for update books to complete before setting statee

--- src/App.js
+++ src/App.js
@@ -16,42 +16,29 @@ class BooksApp extends React.Component {
     search: [],
   };
 
-  componentDidMount() {
+  fetchBooks() {
     BooksAPI.getAll().then(books => {
       this.setState({ books });
     });
   }
 
-  // updateBook(title, bookAttributes) {
-  //   const index = this.state.books.findIndex(b => b.title === title);
-  //   if (index === -1) {
-  //     console.log('index not found');
-  //   } else {
-  //     console.log('setting state');
-  //     this.setState(
-  //       {
-  //         books: [
-  //           ...this.state.books.slice(0, index),
-  //           Object.assign(this.state.books[index], bookAttributes),
-  //           ...this.state.books.slice(index + 1),
-  //         ],
-  //       },
-  //       () => {
-  //         console.log(this.state.books);
-  //         console.log(this.state.books[index]);
-  //       },
-  //     );
-  //   }
-  // }
+  componentDidMount() {
+    this.fetchBooks();
+  }
+
+  updateBook = (id, category) => {
+    console.log('state before --> ', this.state);
+    BooksAPI.update(id, category).then(() => {
+      this.fetchBooks();
+    });
+    console.log('state after -->', this.state);
+  };
 
   onCategoryChange = (id, category) => {
-    // const indexOfBook = this.state.books.findIndex(b => b.title === bookTitle);
-    // console.log(indexOfBook);
     console.log('app id --> ', id);
     console.log('app category --> ', category);
     console.log('state --> ', this.state.books);
-    // this.updateBook(bookTitle, { category: category });
-    BooksAPI.update(id, category);
+    this.updateBook(id, category);
   };
 
   searchBook = query => {
@@ -63,7 +50,6 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {/* {this.state.showSearchPage ? ( */}
         <Route
           path="/search"
           render={() => (
@@ -94,7 +80,6 @@ class BooksApp extends React.Component {
           )}
         />
 
-        {/* ) : ( */}
         <Route
           exact
           path="/"
@@ -131,68 +116,9 @@ class BooksApp extends React.Component {
             </div>
           )}
         />
-
-        {/* )} */}
       </div>
     );
   }
 }
 
 export default BooksApp;
-
-// {
-//   title: `To Kill a Mockingbird`,
-//   author: 'Harper Lee',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
-//   imageHeight: 193,
-//   category: 'currently-reading',
-// },
-// {
-//   title: `Ender's Game`,
-//   author: 'Orson Scott Card',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
-//   imageHeight: 188,
-//   category: 'currently-reading',
-// },
-// {
-//   title: `1776`,
-//   author: 'David McCullough',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-//   imageHeight: 193,
-//   category: 'want-to-read',
-// },
-// {
-//   title: `Harry Potter and the Sorcerer's Stone`,
-//   author: 'JK Rowling',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
-//   imageHeight: 192,
-//   category: 'want-to-read',
-// },
-// {
-//   title: `The Hobbit`,
-//   author: 'JRR Tolkien',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
-//   imageHeight: 192,
-//   category: 'read',
-// },
-// {
-//   title: `Oh, the Places You'll Go!`,
-//   author: 'Suess',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
-//   imageHeight: 172,
-//   category: 'read',
-// },
-// {
-//   title: `The Adventures of Tom Sawyer`,
-//   author: 'Mark Twain',
-//   imageUrl:
-//     'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_apis',
-//   imageHeight: 192,
-//   category: 'read',
-// },
